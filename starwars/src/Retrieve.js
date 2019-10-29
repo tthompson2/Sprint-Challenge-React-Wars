d17ba67c2d91d71373c1bc5aeee7936af6298e60
Trevor Thompson <trevthom18@gmail.com>
2019-10-28 21:07:09 -0400

    All my elements are properly displaying now

--- starwars/src/Retrieve.js
+++ starwars/src/Retrieve.js
@@ -24,15 +24,15 @@ export default function Retrieve() {
     return (
       <div>
         {
-          Object.keys(value).map(element => (
-            <SWcard name={value.name}
-              height={value.height}
-              mass={value.mass}
-              hair_color={value.hair_color}
-              skin_color={value.skin_color}
-              birth_color={value.eye_color}
-              gender={value.gender}
-              homeworld={value.homeworld}
+          value.map(element => (
+            <SWcard name={element.name}
+              height={element.height}
+              mass={element.mass}
+              hair_color={element.hair_color}
+              skin_color={element.skin_color}
+              birth_color={element.eye_color}
+              gender={element.gender}
+              homeworld={element.homeworld}
               />)
         )
         }
